0b5d17e7d8af72882cb5a9ac89488908f866f9bd
HenryXin <yuepeng.xin@gmail.com>
2024-12-14 15:18:20 -0500

    add adc module

--- Application/src/main.c
+++ Application/src/main.c
@@ -15,6 +15,23 @@
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/adc.h>
+
+
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified"
+#endif
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+/* Data of ADC io-channels specified in devicetree. */
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
+
 
 #define SW0_NODE	DT_ALIAS(sw0)
 #define I2C0_NODE DT_NODELABEL(colorsensor)
@@ -143,6 +160,29 @@ int main(void)
 	uint32_t period;
 	uint8_t dir = 0U;
 	int ret;
+
+	uint32_t count = 0;
+	uint16_t buf;
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+		if (!adc_is_ready_dt(&adc_channels[i])) {
+			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+			return 0;
+		}
+
+		ret = adc_channel_setup_dt(&adc_channels[i]);
+		if (ret < 0) {
+			printk("Could not setup channel #%d (%d)\n", i, ret);
+			return 0;
+		}
+	}
+
 
 	printk("PWM-based blinky\n");
 
@@ -291,6 +331,42 @@ int main(void)
 		}
 		ReadColor();
 		TM1650_displayTest();
+
+		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+			int32_t val_mv;
+
+			printk("- %s, channel %d: ",
+			       adc_channels[i].dev->name,
+			       adc_channels[i].channel_id);
+
+			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+
+			ret = adc_read_dt(&adc_channels[i], &sequence);
+			if (ret < 0) {
+				printk("Could not read (%d)\n", ret);
+				continue;
+			}
+
+			/*
+			 * If using differential mode, the 16 bit value
+			 * in the ADC sample buffer should be a signed 2's
+			 * complement value.
+			 */
+			if (adc_channels[i].channel_cfg.differential) {
+				val_mv = (int32_t)((int16_t)buf);
+			} else {
+				val_mv = (int32_t)buf;
+			}
+			printk("%"PRId32, val_mv);
+			ret = adc_raw_to_millivolts_dt(&adc_channels[i],
+						       &val_mv);
+			/* conversion to mV may not be supported, skip if not */
+			if (ret < 0) {
+				printk(" (value in mV not available)\n");
+			} else {
+				printk(" = %"PRId32" mV\n", val_mv);
+			}
+		}
 		k_sleep(K_SECONDS(4U));
 	}
 	return 0;
